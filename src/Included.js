aea48b01ca3e4a4f81e1817d35bccf477e8ec2a1
Aaron Bullard <aaron@2book.com>
2018-02-17 18:29:03 -0500

    refactored Included - faster

--- src/Included.js
+++ src/Included.js
@@ -4,38 +4,17 @@ function Included(arr) {
 
   var _included = arr || [];
 
-  var _mapped = [];
-
-  var _cache = {};
-
-  var _cacheCount = {};
-
-  var _getIncluded = () => {
-    if(!_mapped.length){
-      _mapped = _included.map(resobj => new ResourceObject(resobj));
-    }
-
-    return _mapped;
-  }
-
   var _getKey = (type, id) => (`${type}-${id}`);
 
-  var _cacheFindOrCreate = (key, callable) => {
-    if(!_cache.hasOwnProperty(key)){
-      _cache[key] = callable();
-    }
-
-    return _cache[key];
-  }
+  var _index = _included.reduce((mapping, obj) => {
+    mapping[_getKey(obj.type, obj.id)] = new ResourceObject(obj);
+    return mapping;
+  }, {});
 
   var findResourceObject = (type, id) => {
-    let object = _cacheFindOrCreate(_getKey(type, id), () => {
-      return _getIncluded().find((resource) => {
-        return resource.getType() == type && resource.getId() == id;
-      });
-    });
+    let key = _getKey(type, id);
 
-    return typeof object == "undefined" ? null : object;
+    return _index.hasOwnProperty(key) ? _index[key] : null;
   }
 
   return {
